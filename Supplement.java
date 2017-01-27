cb98ddae66cfef099818717775f688f959818eac
Michael Chan-Pong <Michael.Chan-Pong@outlook.com>
2017-01-27 18:41:21 -0500

    this is fun

--- Supplement.java
+++ Supplement.java
@@ -1,6 +1,6 @@
-public class main{
+public class Supplement{
 	public static void main(String[] args){
 		System.out.println("Good job Conor");
-		System.out.println("Notice me Conor");
+		System.out.println("That was cool");
 	}
 }
