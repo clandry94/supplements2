f9718d81f3e56fbec809bc7402b62cdb23c0a8a6
Michael Chan-Pong <Michael.Chan-Pong@outlook.com>
2017-01-27 18:50:26 -0500

    run this

--- Supplement.java
+++ Supplement.java
@@ -1,5 +1,10 @@
-public class main{
+public class Supplement{
 	public static void main(String[] args){
-		System.out.println("Good job Conor");
+		System.out.println("Good job Conor!");
+		System.out.println("Sign in if you haven't yet!");
+		System.out.println("Complements: next Thursday, 6:15 in LIT221");
+		System.out.println("ACM General Body Meeting: Feb 2nd, 6:15 in CSE E222");
+		System.out.println("Next Supplements: Feb 10th with DAS, location TBD");
+		System.out.println("Friday Night Hacks: now in the Fishbowl!");
 	}
 }
